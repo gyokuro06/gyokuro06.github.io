97f521523fb20c828d85a59fbb22b5e3ef9b79b9
gyokuro06 <gyokuro06@gmail.com>
2026-06-29 23:18:54 +0900

    fix: base

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://gyokuro06.github.io',
-  base: '/sleepy-turtle',
+  base: '/blog',
   integrations: [react()]
 });
